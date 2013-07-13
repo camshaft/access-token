034414883d5305e4b48b07ea87d9437643f56f0d
Cameron Bytheway <cameron@nujii.com>
2013-07-13 15:34:53 -0600

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,30 @@ access-token
 ============
 
 Get/format an access token
+
+Installation
+------------
+
+```sh
+$ component install CamShaft/access-token
+```
+
+API
+---
+
+```js
+var token = require('access-token');
+
+token();
+// my-access-token
+
+token.bearer();
+// Bearer my-access-token
+
+token.auth();
+// {authorization: 'Bearer my-access-token'}
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,46 @@
+/**
+ * Module dependencies
+ */
+
+var cookie = require('cookie');
+
+/**
+ * Set the default name
+ */
+
+var name = '_access_token';
+
+/**
+ * Get the access token cookie
+ *
+ * @return {String}
+ * @api public
+ */
+
+module.exports = exports = function() {
+  return cookie(exports.name || name);
+};
+
+/**
+ * Get the access token in the form of a bearer token
+ *
+ * @return {String}
+ * @api public
+ */
+
+exports.bearer = function() {
+  var accessToken = exports();
+  return accessToken ? 'Bearer '+accessToken : undefined;
+};
+
+/**
+ * Get the access token in the form of a header options
+ *
+ * @return {Object}
+ * @api public
+ */
+
+exports.auth = function() {
+  var bearer = exports.bearer();
+  return bearer ? {'authorization': bearer} : {};
+};
